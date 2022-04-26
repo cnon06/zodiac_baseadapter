5ab841083349dc62a12ce1cafa4702ab4966788b
sinan <sinanalagoz74@gmail.com>
2022-04-26 09:42:35 +0300

    dd

--- app/src/main/java/com/cnon/zodiac/burclarArrayAdapter.kt
+++ app/src/main/java/com/cnon/zodiac/burclarArrayAdapter.kt
@@ -2,10 +2,13 @@ package com.cnon.zodiac
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import kotlinx.android.synthetic.main.single_row.view.*
 
 
@@ -20,27 +23,50 @@ class zodiacArrayAdapter (var getContext: Context, resource: Int, textViewResour
     @SuppressLint("ViewHolder")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
+        var single_row_view = convertView
+        var viewHolder:viewHolder
 
-    //    binding = ActivityHomeBinding.inflate(layoutInflater)
-     //   setContentView(binding.root)
+        if(single_row_view==null)
+        {
+            val inflater = LayoutInflater.from(getContext)
+            single_row_view = inflater.inflate(R.layout.single_row,parent,false)
+
+            viewHolder = viewHolder(single_row_view)
+            single_row_view.tag=viewHolder
+
+        }
+
+        else
+        {
+            viewHolder = single_row_view.getTag() as viewHolder
 
 
-        val inflater = LayoutInflater.from(getContext)
-        val single_row_view = inflater.inflate(R.layout.single_row,parent,false)
+        }
 
-        var zodiacImageView = single_row_view.imgZodiacSymbol
-        var zodiacName = single_row_view.tvZodiacName
-        var zodiacDate2 = single_row_view.tvZodiacDate
+        viewHolder.zodiacImageView.setImageResource(zodiacImage[position]);
+        viewHolder.zodiacName.setText(zodiac[position])
+        viewHolder.zodiacDate.setText(zodiacDate[position])
 
+        Log.e("Inflater",zodiacDate[position])
 
-        zodiacImageView.setImageResource(zodiacImage[position])
-        zodiacName.setText(zodiac[position])
-        zodiacDate2.setText(zodiacDate[position])
-
-
-        return single_row_view
+        return single_row_view!!
     }
 
+
+    class viewHolder(single_row_view: View)
+    {
+            var zodiacImageView : ImageView
+            var zodiacName : TextView
+            var zodiacDate : TextView
+
+            init {
+                this.zodiacImageView = single_row_view.imgZodiacSymbol
+                this.zodiacName = single_row_view.tvZodiacName
+                this.zodiacDate = single_row_view.tvZodiacDate
+            }
+
+
+    }
 
 
 }
